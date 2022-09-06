e27f8d54fcfc857e416e31f7c99ed5931ad620c6
mustapelto <46607498+mustapelto@users.noreply.github.com>
2022-09-06 13:41:31 +0300

    Added features, simplified

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # string-patternizer
+
+## Overview
+
+---
+
+Inspired by (but not copied from) [https://github.com/arthurdenner/format-string-by-pattern](format-string-by-pattern). Converts any string to match a given pattern. Currently supports
+letters (case-sensitive or -insensitive), digits, symbols, "any character" and specific/required characters.
+
+## Install
+
+---
+
+    yarn add string-patternizer
+
+    npm i string-patternizer
+
+    pnpm add string-patternizer
+
+## Usage
+
+---
+
+### patternize(pattern, source)
+
+Returns a `string` based on `source`, formatted according to `pattern`.
+
+Note that this is *not* a validation tool: it will only convert strings according to the pattern provided, but not otherwise check for their content.
+In other words, no function is provided to e.g. limit an IP address string to numbers smaller than 256.
+
+**pattern**: `string`
+
+Describes how the input should be formatted.
+
+Characters with special meaning:
+- d: any digit
+- x: any letter
+- a: any lowercase letter
+- A: any uppercase letter
+- *: any "other" (not digit or letter) character
+- .: any character
+- +: repeat previous 0-infinite times
+- \: start of escape sequence
+
+Any other character will be interpreted as "required", i.e. will automatically be copied *as-is* into the result string, even if not present in the source string.
+The special characters above need to be escaped to be used as "required".
+
+**source**: `string`
+
+The input value to be formatted.
+
+### Example usage:
+
+    import patternize from 'string-patternizer'
+
+    // US phone number (including country code)
+    patternize('5551234567', '\+1 (ddd) ddd dddd');
+    // '+1 (555) 123 4567'
+
+    // Full name
+    patternize('firstname lastname', 'Aa+ Aa+');
+    // 'Firstname Lastname'
+
+    // IP address (format only, no validation i.e. numbers over 255 are possible)
+    patternize('111222333444', 'ddd\.ddd\.ddd\.ddd');
+    // '111.222.333.444'
+
+    // Currency
+    patternize('1234', 'd+\.dd€');
+    // '12.34€'
--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,158 @@
-type CharType = 'number' | 'letter' | 'other';
+/*
+*
+* x - any letter
+* a - any lowercase letter
+* A - any uppercase letter
+* d - any digit
+* * - any symbol
+* . - any character
+* + - repeat preceding character 0-x times
+* \ - escape sequence
+* anything else - that character
+* escape + any above - that character
+*
+*/
 
-const isNumber = (value: string) => /\d/.test(value);
+enum CharType {
+    Digit,
+    Letter,
+    Lowercase,
+    Uppercase,
+    Symbol,
+    Any,
+    Repeat,
+    Escape,
+    Specific
+}
+
+const isDigit = (value: string) => /\d/.test(value);
 const isLetter = (value: string) => /[A-Z]/i.test(value);
-const isOther = (value: string) => /[^0-9A-Z]/i.test(value);
+const isLowercaseLetter = (value: string) => /[a-z]/.test(value);
+const isUppercaseLetter = (value: string) => /[A-Z]/.test(value);
 
-const getCharType = (char: string): CharType | undefined => {
-    if (char.length === 0 || char.length > 1)
-        return undefined;
-    if (isNumber(char))
-        return 'number';
-    if (isLetter(char))
-        return 'letter';
-    if (isOther(char))
-        return 'other';
-    return undefined;
+const getSourceCharType = (char: string, ignoreLetterCase?: boolean): CharType => {
+    if (char.length !== 1)
+        throw new SyntaxError(`Syntax error: getSourceCharType(${char}). Argument must be a string of length 1.`);
+
+    if (isDigit(char))
+        return CharType.Digit;
+    if (ignoreLetterCase && isLetter(char))
+        return CharType.Letter;
+    if (isLowercaseLetter(char))
+        return CharType.Lowercase;
+    if (isUppercaseLetter(char))
+        return CharType.Uppercase;
+    return CharType.Symbol;
 }
 
-const getCharTypes = (value: string) => {
-    let result: string[] = [];
+const getPatternCharType = (char: string): CharType => {
+    if (char.length !== 1)
+        throw new SyntaxError(`Syntax error: getPatternCharType(${char}). Argument must be a string of length 1.`);
 
-    for (let i = 0; i < value.length; i++) {
-        const def = getCharType(value[i]);
-
-        if (def)
-            result.push(def);
+    switch (char) {
+        case 'd': return CharType.Digit;
+        case 'x': return CharType.Letter;
+        case 'a': return CharType.Lowercase;
+        case 'A': return CharType.Uppercase;
+        case '*': return CharType.Symbol;
+        case '.': return CharType.Any;
+        case '+': return CharType.Repeat;
+        case '\\': return CharType.Escape;
+        default: return CharType.Specific;
     }
-
-    return result;
 }
 
-export const patternize = (source: string, pattern: string) => {
-    const sourceTypes = getCharTypes(source);
-    const patternTypes = getCharTypes(pattern);
+const isLetterType = (charType?: CharType) => charType && ((charType === CharType.Letter) || (charType === CharType.Lowercase) || (charType === CharType.Uppercase));
 
-    if (sourceTypes.length !== source.length) {
-        throw new Error('Invalid patternize input: source contains invalid characters!');
-    }
+const fitsPattern = (sourceType: CharType, patternType: CharType) =>
+    (patternType === CharType.Any) || 
+    (patternType === sourceType) ||
+    (isLetterType(patternType) && isLetterType(sourceType));
+
+const transformCaseIfRequired = (char: string, patternType: CharType) => {
+    if (char.length !== 1)
+        throw new Error(`Syntax error: transformCaseIfRequired(${char}, ${patternType}). First argument must be a string of length 1.`);
     
-    if (patternTypes.length !== pattern.length) {
-        throw new Error('Invalid patternize input: pattern contains invalid characters!');
-    }
+    if (patternType === CharType.Lowercase)
+        return char.toLocaleLowerCase();
+    else if (patternType === CharType.Uppercase)
+        return char.toLocaleUpperCase();
+    else
+        return char;
+}
 
+export const patternize = (pattern: string, source: string) => {
     let sourceIndex = 0;
     let patternIndex = 0;
 
     let result = '';
 
-    while (sourceIndex < sourceTypes.length && patternIndex < patternTypes.length) {
-        const sourceType = sourceTypes[sourceIndex];
-        const patternType = patternTypes[patternIndex];
-        const sourceChar = source[sourceIndex];
-        const patternChar = pattern[patternIndex];
+    let patternType: CharType;
+    let prevPatternType: CharType | undefined = undefined;
 
-        if (sourceType === patternType) {
-            // same type in source and pattern --> copy char and advance both indices
-            result += sourceChar;
-            sourceIndex++;
-            patternIndex++;
-        } else if (patternTypes[patternIndex] === 'other') {
-            // pattern contains "delimiter" char not in source --> copy pattern char and advance pattern index
-            result += patternChar;
-            patternIndex++;
+    while (patternIndex < pattern.length) {
+        const sourceChar = source[sourceIndex] || undefined;
+        const patternChar = pattern[patternIndex];
+        
+        if (prevPatternType === CharType.Escape) {
+            patternType = CharType.Specific;
         } else {
-            // source doesn't match pattern --> jump over
-            sourceIndex++;
+            patternType = getPatternCharType(patternChar);
         }
+
+        if (patternType === CharType.Escape) {
+            // Start of escape sequence --> advance pattern index to read whole sequence
+            patternIndex++;
+        } else if (patternType === CharType.Specific) {
+            // pattern demands specific character
+            // - check if source has same character
+            // - if yes: advance both indices
+            // - if no: advance only pattern index (= character gets inserted)
+            // - both cases: copy character from pattern string
+
+            if (sourceChar === patternChar) {
+                sourceIndex++;
+            }
+            patternIndex++;
+            result += patternChar;
+        } else {
+            if (!sourceChar)
+                break; // Source ended before pattern, pattern contains remaining non-specific characters --> incomplete input, abort processing
+        
+            const sourceType = getSourceCharType(sourceChar, patternType === CharType.Letter);
+
+            if (patternType === CharType.Repeat) {
+                if (!prevPatternType) {
+                    throw new SyntaxError(`Invalid pattern: ${pattern}. "Repeat" ('+') not allowed at pattern start.`);
+                }
+
+                if (fitsPattern(sourceType, prevPatternType)) {
+                    // current char still fits repeat pattern:
+                    // - copy char and transform case if necessary
+                    // - advance source index
+                    // - skip prevPatternType update
+                    result += transformCaseIfRequired(sourceChar, prevPatternType);
+                    sourceIndex++;
+                    continue;
+                } else {
+                    // current char no longer fits repeat pattern:
+                    // - advance pattern index
+                    patternIndex++;
+                }
+            } else if (fitsPattern(sourceType, patternType)) {
+                // same type in source and pattern, or pattern allows any character:
+                // - copy char and convert case if necessary
+                // - advance both indices
+                result += transformCaseIfRequired(sourceChar, patternType);
+                sourceIndex++;
+                patternIndex++;
+            } else {
+                // source doesn't match pattern --> ignore character
+                sourceIndex++;
+            }
+        }
+
+        prevPatternType = patternType;
     }
 
     return result;
